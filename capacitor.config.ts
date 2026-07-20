d15cf2df510e3958715cea1c3c17f999556ef760
rajeshkpallywear <rajeshkpallywear@gmail.com>
2026-07-20 20:48:13 +0530

    feat: update application and main activity labels to pallywear.in

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'pallywear.com',
+  appId: 'pallywear.in',
   appName: 'pallywear',
   webDir: 'dist'
 };
